f56145fb855417a7ba2c14a998e413f525620070
LuSrodri <lucassrodri08@gmail.com>
2022-01-23 23:38:19 -0300

    useContext part 1

--- src/views/examples/UseContext.jsx
+++ src/views/examples/UseContext.jsx
@@ -1,13 +1,35 @@
-import React from 'react'
+
+
+import React, { useContext } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
 
+import DataContext from '../../data/dataContext'
+
 const UseContext = (props) => {
+
+    let context = useContext(DataContext)
+
+    function setNumber(n){
+        context.setState({
+            ...context.state,
+            number: n
+        })
+    }
+
     return (
         <div className="UseContext">
             <PageTitle
-                title="Hook UseContext"
-                subtitle="Aceita um objeto de contexto e retorna o valor atual do contexto!"
+                title="Hook UseContext Context API"
+                subtitle="Accepts a Context object and return the current value of Context."
             />
+            <div className="center">
+                <span className='text'>{context.state.number} - {context.state.text}</span>
+            </div>
+
+            <div className='center'>
+                <button className='btn' onClick={ ()=> setNumber(context.state.number-1)}>-1</button>
+                <button className='btn' onClick={ ()=> setNumber(context.state.number+1)}>+1</button>
+            </div>
         </div>
     )
 }
